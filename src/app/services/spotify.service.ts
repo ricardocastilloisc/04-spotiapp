1ed9c3be963e320923dbf2e108a365cb0a0ece1a
ricardocastilloisc <ricardocastilloisc@gmail.com>
2018-11-15 12:06:22 -0500

    6.13. Centralizar las peticiones hacia Spotify

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -8,39 +8,40 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class SpotifyService {
-  private url = 'Bearer BQAfQNwVa8IolOr9ECTnlZyDmbG1omzpbzNnQwn7pkN4gvWVqU2mmdLUN3Sk7neHydKeBOFlRL7YyxUKt3c';
 
 
-  private
+
+  private token = 'Bearer BQAfQNwVa8IolOr9ECTnlZyDmbG1omzpbzNnQwn7pkN4gvWVqU2mmdLUN3Sk7neHydKeBOFlRL7YyxUKt3c';
+
+
   constructor(private http: HttpClient) {
 
     this.getNewReleses();
 
   }
-
-  getNewReleses() {
-
-    // tslint:disable-next-line:max-line-length
-
+  getQuery(query: string) {
 
     const headers = new HttpHeaders(
       {
         // tslint:disable-next-line:max-line-length
-        'Authorization': this.url
+        'Authorization': this.token
       });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers}).pipe(
+
+    const url = `https://api.spotify.com/v1/${ query }`;
+
+
+    return this.http.get(url, {headers});
+  }
+
+  getNewReleses() {
+
+    return this.getQuery('browse/new-releases').pipe(
       map( data =>   data['albums'].items ));
   }
 
   getArtista(termino: string){
-    const headers = new HttpHeaders(
-      {
-        // tslint:disable-next-line:max-line-length
-        'Authorization': this.url
-      });
-
-    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers}).pipe(
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`).pipe(
       map( data =>  data['artists'].items)
     );
   }
